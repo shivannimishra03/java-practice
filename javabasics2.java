593943acb1242e33b87ae4698d1bcfbbefb3b28e
Shivani <shivannimishra03@gmail.com>
2024-01-17 20:50:11 +0530

    primative data type

---
+++ javabasics2.java
@@ -0,0 +1,19 @@
+public class javabasics2 {
+    public static void main(String args[]){
+
+        //primative date types 
+
+        byte b = 8;
+        System.out.println(b);
+        char ch = 'a';
+        System.out.println(ch);
+        //boolean var = true;
+        //float price = 1.5;
+        //System.out.println(price);
+        //int number = 25;
+        //long
+        //double 
+        //short n = 240;
+    }
+    
+}
