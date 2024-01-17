1b1b1af03414b966aef1c006cc0fc1a7c58c85d5
Shivani <shivannimishra03@gmail.com>
2024-01-17 16:04:02 +0530

    add more codes line

--- javabasics.java
+++ javabasics.java
@@ -2,5 +2,16 @@ public class javabasics{
     
     public static void main(String args[]){
         System.out.print("Hello World!");
+        System.out.print("hi Shivani\n");   //next line
+        System.out.println("hello world");  // one line space
+        System.out.println("hello world");   // one line space
+        System.out.println("hello world");   // one line space
+        System.out.print("hi Shivani\nhow are you\nWelcome to Java\n");     
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.print("*");
+
+
     }
 }
