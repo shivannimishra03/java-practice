fd81d82428c19e3d16892b4051023e9b19c88528
Shivani <shivannimishra03@gmail.com>
2024-01-18 22:25:30 +0530

    sum of two number

---
+++ javabasics6.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class javabasics6 {
+    public static void main(String[] args) {
+        //sum of two numbers
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b ;
+        System.out.println(sum);
+    }
+    
+}
