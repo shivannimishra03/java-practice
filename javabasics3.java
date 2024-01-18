8fda46b9f660569ba7d7c429328d4b514cc05bf7
Shivani <shivannimishra03@gmail.com>
2024-01-18 21:54:11 +0530

    sum of a and b

---
+++ javabasics3.java
@@ -0,0 +1,10 @@
+public class javabasics3 {
+    public static void main(String[] args) {
+      //sum of a and b 
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+        System.out.println(sum);
+    }
+    
+}
