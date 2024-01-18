221e3b72fe920e8048bc75485dbdcff013cf495e
Shivani <shivannimishra03@gmail.com>
2024-01-18 22:02:22 +0530

    comments

---
+++ javabasics4.java
@@ -0,0 +1,21 @@
+public class javabasics4 {
+    public static void main(String[] args) {
+        //comments
+
+        //sum of a and b    // single line comment 
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+        System.out.println(sum);
+        
+        /*
+         hello
+         hello world 
+         this is a multi line comment 
+         
+         */
+    
+        
+    }
+    
+}
