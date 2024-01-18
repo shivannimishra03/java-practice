16a2d08dace00af92aff3d3e37f79db20a1e20dd
Shivani <shivannimishra03@gmail.com>
2024-01-18 22:17:49 +0530

    input types

---
+++ javabasics5.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class javabasics5 {
+    public static void main(String[] args) {
+        // input data types
+        Scanner sc = new Scanner(System.in);  // code to print first word of name
+        String input = sc.next();
+        System.out.println(input);
+
+        String name = sc.nextLine();   //code to store or wright full name 
+        System.out.println(name);
+
+        int number = sc.nextInt();
+        System.out.println(number);
+
+        float price = sc.nextFloat();
+        System.out.println(price);
+
+    }
+    
+}
