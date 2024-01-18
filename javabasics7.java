f624673f0f70fcc7c55eb8769e5766619bf95918
Shivani <shivannimishra03@gmail.com>
2024-01-18 22:29:48 +0530

    product of two numbers

---
+++ javabasics7.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class javabasics7 {
+    public static void main(String[] args) {
+        // product of two number
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int product = a * b ;
+        System.out.println(product);
+    }
+    
+}
