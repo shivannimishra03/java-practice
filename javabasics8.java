005e4a8fcea79747a4ed7599f4d17f3c2e22d331
Shivani <shivannimishra03@gmail.com>
2024-01-21 12:16:33 +0530

    some codes

---
+++ javabasics8.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class javabasics8 {
+    public static void main(String[] args) {
+        //area of circle
+        Scanner sc = new Scanner(System.in);
+        float rad = sc.nextFloat();
+        float area = 3.14f * rad * rad;
+        System.out.println(area);
+    }
+    
+}
---
+++ javabasics9.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class javabasics9 {
+    //type conversion
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float number = sc.nextInt();
+        System.out.println(number);
+    }
+}
